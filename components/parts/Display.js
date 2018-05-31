cff6bb049fb1061923a3b519d3617682a9c526d0
jamesliauw <jamesliauw@163.com>
2018-05-31 14:56:32 +0800

    feat: remove crash

--- components/parts/Display.js
+++
@@ -1,9 +0,0 @@
-var React = require('react');
-
-var Display = React.createClass({
-	render() {
-		return (this.props.if) ? <div>{this.props.children}</div> : null;
-	}
-});
-
-module.exports = Display;
