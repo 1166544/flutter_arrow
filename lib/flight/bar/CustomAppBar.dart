8c635142a774b8ba6cd550a359a824ccff0e4f58
James Liauw <jamesliauw@163.com>
2019-01-03 13:59:34 +0800

    feat: add app bar common

--- lib/flight/bar/CustomAppBar.dart
+++ lib/flight/bar/CustomAppBar.dart
@@ -3,28 +3,40 @@ import 'package:flighttickets/flight/styles/CustomeStyle.dart';
 
 /// 自定义APPBAR
 class CustomAppBar extends StatelessWidget {
-	final List<BottomNavigationBarItem> bottomBarItems = [];
-	final bottomNavigationBarItemStyle = TextStyle(fontStyle: FontStyle.normal, color: Colors.black);
+
+	/// TAB列表数据
+	List<BottomNavigationBarItem> bottomBarItems;
 
 	CustomAppBar() {
-		BottomNavigationBarItem bottomBarItemExplore = BottomNavigationBarItem(
-			icon: Icon(Icons.home, color: appTheme.primaryColor),
-			title: Text('Explore', style: this.bottomNavigationBarItemStyle.copyWith(color: appTheme.primaryColor))
-		);
-		BottomNavigationBarItem bottomBarItemWatchList = BottomNavigationBarItem(
-			icon: Icon(Icons.favorite, color: Colors.black),
-			title: Text('Whatlist', style: this.bottomNavigationBarItemStyle)
-		);
-		BottomNavigationBarItem bottomBarItemNotifications = BottomNavigationBarItem(
-			icon: Icon(Icons.notifications, color: Colors.black),
-			title: Text('Notifications', style: this.bottomNavigationBarItemStyle)
-		);
+		// 默认样式
+		TextStyle defaultBarItemStyle = TextStyle(fontStyle: FontStyle.normal, color: Colors.black);
 
-		this.bottomBarItems.add(bottomBarItemExplore);
-		this.bottomBarItems.add(bottomBarItemWatchList);
-		this.bottomBarItems.add(bottomBarItemNotifications);
+		// 选中样式
+		TextStyle selectedBarItemStyle = defaultBarItemStyle.copyWith(color: appTheme.primaryColor);
+
+		// 添加TAB数据
+		this.bottomBarItems = [
+			this.getNavigationBarItem(Icons.home, appTheme.primaryColor, 'Explore', selectedBarItemStyle),
+			this.getNavigationBarItem(Icons.favorite, appTheme.primaryColor, 'Whatlist', defaultBarItemStyle),
+			this.getNavigationBarItem(Icons.bookmark, appTheme.primaryColor, 'Deals', defaultBarItemStyle),
+			this.getNavigationBarItem(Icons.notifications, appTheme.primaryColor, 'Notifications', defaultBarItemStyle)
+		];
 	}
 
+	/**
+	 * 构造TAB ITEM内容
+	 * @params icon 图标
+	 * @params color 图标颜色
+	 * @params label 文字
+	 * @params style 文样样式
+	 */
+	BottomNavigationBarItem getNavigationBarItem(IconData icon, Color color, String label, TextStyle style) {
+		return BottomNavigationBarItem(
+			icon: Icon(icon, color: color), title: Text(label, style: style)
+		);
+	}
+
+	/// 构造底部样式TAB BAR
 	@override
 	Widget build(BuildContext context) {
 		return Material(
