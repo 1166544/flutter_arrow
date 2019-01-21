1c20dfee4696c146e6377acd73904b7ffc3b9789
James Liauw <jamesliauw@163.com>
2019-01-21 11:42:17 +0800

    feat: 添加子页面

---
+++ lib/flight/notifications/CustomNotifications.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CustomNotifications extends StatelessWidget {
+	@override
+	Widget build(BuildContext context) {
+		return Text('Notifications page');
+	}
+}
