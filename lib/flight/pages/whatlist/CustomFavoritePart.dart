b869210016aa81470e2a1f306383bfb4e9b0e145
James Liauw <jamesliauw@163.com>
2019-02-06 14:07:33 +0800

    feat: frefix star num

--- lib/flight/pages/whatlist/CustomFavoritePart.dart
+++ lib/flight/pages/whatlist/CustomFavoritePart.dart
@@ -14,15 +14,15 @@ class CustomFavoritePart extends StatelessWidget {
 				children: <Widget>[
 					Text('CustomFavoritePart', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
 					SizedBox(height: 20.0),
-					this.buildGuideItem(context, 'assets/images/pic6.png', 'Samantha William', 4.2, 86, 4, 1),
+					this.buildGuideItem(context, 'assets/images/pic6.png', 'Samantha William', 4.2, 86, 4),
 					SizedBox(height: 10.0),
 					this.buildSeperateLine(),
 					SizedBox(height: 10.0),
-					this.buildGuideItem(context, 'assets/images/pic8.png', 'Jonathan Hope', 2.2, 86, 2, 2),
+					this.buildGuideItem(context, 'assets/images/pic8.png', 'Jonathan Hope', 2.2, 86, 2),
 					SizedBox(height: 20.0),
-					this.buildGuideItem(context, 'assets/images/pic9.png', 'Andrea Summers', 3.2, 86, 3, 3),
+					this.buildGuideItem(context, 'assets/images/pic9.png', 'Andrea Summers', 3.2, 86, 3),
 					SizedBox(height: 20.0),
-					this.buildGuideItem(context, 'assets/images/pic10.png', 'William Johnson', 4.2, 86, 4, 4),
+					this.buildGuideItem(context, 'assets/images/pic10.png', 'William Johnson', 4.2, 86, 4),
 				],
 			),
 		);
@@ -44,7 +44,7 @@ class CustomFavoritePart extends StatelessWidget {
 	}
 
 	/// 构建月度评分
-	Widget buildGuideItem(BuildContext context, String imgUrl, String name, double score, int views, int startNum, int index) {
+	Widget buildGuideItem(BuildContext context, String imgUrl, String name, double score, int views, int startNum) {
 		return Row(
 			children: <Widget>[
 				// 用户圆角头像
@@ -70,10 +70,7 @@ class CustomFavoritePart extends StatelessWidget {
 							Text(name),
 							Row(
 								children: <Widget>[
-									this.buildRateStartItem(startNum, index),
-									this.buildRateStartItem(startNum, index),
-									this.buildRateStartItem(startNum, index),
-									this.buildRateStartItem(startNum, index),
+									this.buildRateStartItem(startNum),
 									SizedBox(width: 15.0,),
 									Text(score.toString(), style: TextStyle(fontSize: 11.0)),
 									SizedBox(width: 3.0,),
@@ -88,11 +85,23 @@ class CustomFavoritePart extends StatelessWidget {
 	}
 
 	/// 构建星星效果
-	Widget buildRateStartItem(int startNum, int index) {
-		if (startNum >= 4) {
-			return Icon(Icons.star, color: Colors.yellow, size: 16.0);
-		} else {
-			return Icon(Icons.star, color: Colors.grey, size: 16.0);
-		}
+	Widget buildRateStartItem(int startNum) {
+		return Row(
+			children: <Widget>[
+				this.buildStar(startNum, 1),
+				this.buildStar(startNum, 2),
+				this.buildStar(startNum, 3),
+				this.buildStar(startNum, 4),
+			],
+		);
+	}
+
+	/// 单个星星
+	Widget buildStar(int startNum, int index) {
+		return Icon(
+			Icons.star,
+			color: startNum >= index ? Colors.yellow : Colors.grey,
+			size: 16.0
+		);
 	}
 }
