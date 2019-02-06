ad06b49d3a345767ac69644e4d9154c7df89f22b
James Liauw <jamesliauw@163.com>
2019-02-06 13:51:15 +0800

    feat: add start rate

--- lib/flight/pages/whatlist/CustomFavoritePart.dart
+++ lib/flight/pages/whatlist/CustomFavoritePart.dart
@@ -13,18 +13,21 @@ class CustomFavoritePart extends StatelessWidget {
 				crossAxisAlignment: CrossAxisAlignment.start,
 				children: <Widget>[
 					Text('CustomFavoritePart', style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold)),
-					SizedBox(height: 10.0),
-					this.buildGuideItem('assets/images/pic6.png', 'Samantha William', 4.2, 86, 4, 1),
-					this.buildGuideItem('assets/images/pic8.png', 'Jonathan Hope', 4.2, 86, 2, 2),
-					this.buildGuideItem('assets/images/pic9.png', 'Andrea Summers', 4.2, 86, 3, 3),
-					this.buildGuideItem('assets/images/pic10.png', 'William Johnson', 4.2, 86, 4, 4),
+					SizedBox(height: 20.0),
+					this.buildGuideItem(context, 'assets/images/pic6.png', 'Samantha William', 4.2, 86, 4, 1),
+					SizedBox(height: 20.0),
+					this.buildGuideItem(context, 'assets/images/pic8.png', 'Jonathan Hope', 2.2, 86, 2, 2),
+					SizedBox(height: 20.0),
+					this.buildGuideItem(context, 'assets/images/pic9.png', 'Andrea Summers', 3.2, 86, 3, 3),
+					SizedBox(height: 20.0),
+					this.buildGuideItem(context, 'assets/images/pic10.png', 'William Johnson', 4.2, 86, 4, 4),
 				],
 			),
 		);
 	}
 
 	/// 构建月度评分
-	Widget buildGuideItem(String imgUrl, String name, double score, double views, double startNum, double index) {
+	Widget buildGuideItem(BuildContext context, String imgUrl, String name, double score, int views, int startNum, int index) {
 		return Row(
 			children: <Widget>[
 				// 用户圆角头像
@@ -42,28 +45,37 @@ class CustomFavoritePart extends StatelessWidget {
 						),
 					),
 				),
-				SizedBox(width: 5.0),
-				Column(
-					children: <Widget>[
-						Text(name),
-						Row(
-							children: <Widget>[
-								Icon(Icons.star, color: startNum < index ? Colors.yellow : Colors.grey),
-								Icon(Icons.star, color: startNum < index ? Colors.yellow : Colors.grey),
-								Icon(Icons.star, color: startNum < index ? Colors.yellow : Colors.grey),
-								Icon(Icons.star, color: startNum < index ? Colors.yellow : Colors.grey),
-								Text(score.toString()),
-								Text(views.toString())
-							],
-						)
-					],
+				Padding(
+					padding: EdgeInsets.only(left: 20.0, top: 0.0),
+					child: Column(
+						crossAxisAlignment: CrossAxisAlignment.start,
+						children: <Widget>[
+							Text(name),
+							Row(
+								children: <Widget>[
+									this.buildRateStartItem(startNum, index),
+									this.buildRateStartItem(startNum, index),
+									this.buildRateStartItem(startNum, index),
+									this.buildRateStartItem(startNum, index),
+									SizedBox(width: 15.0,),
+									Text(score.toString(), style: TextStyle(fontSize: 11.0)),
+									SizedBox(width: 3.0,),
+									Text('(${views.toString()} reviews)', style: TextStyle(fontSize: 11.0, color: Colors.grey))
+								],
+							)
+						],
+					),
 				),
-				Container(
-					width: double.infinity,
-					height: 1.0,
-					color: index == 1 ? Colors.grey : Colors.white
-				)
 			],
 		);
+	}
+
+	/// 构建星星效果
+	Widget buildRateStartItem(int startNum, int index) {
+		if (startNum >= 4) {
+			return Icon(Icons.star, color: Colors.yellow, size: 16.0);
+		} else {
+			return Icon(Icons.star, color: Colors.grey, size: 16.0);
+		}
 	}
 }
