ed24789a8bab287a10ae1ede9c436084f1c4e4b1
williamliao <williamliao@tencent.com>
2021-09-05 18:53:04 +0800

    feat: init ext

---
+++ lib/ext/Ext.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Ext extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return new MaterialApp(
+      title: title,
+      home: new Scaffold(
+          appBar: new AppBar(title: new Text(title)),
+          body: this.getScaffoldBody(context)),
+    );
+  }
+
+  Widget getScaffoldBody(BuildContext context) {
+	  return new Container(
+		  child: Text(
+			  'Test',
+			  textAlign: Textlign.center
+		  )
+	  );
+  }
+}
